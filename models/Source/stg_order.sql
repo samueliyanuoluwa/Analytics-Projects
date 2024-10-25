0831343d7be603f32f4e6efaa7ede2888706244c
Samuel Arojojoye <samuelarojojoye2@gmail.com>
2024-10-25 14:07:02 +0000

    latest

--- models/Source/stg_order.sql
+++ models/Source/stg_order.sql
@@ -1,6 +1,25 @@
 select
-    Contentid,
+    Businessid,
+    Contentid as Orderid,
+    MasterOrderid,
+    Orderstatusid
     Customerid,
     Userid,
-    TO_TIMESTAMP(Createddate) AS Createddate
+    TO_TIMESTAMP(Createddate) AS Createddate,
+    Orderlocationid,
+    Ordersource,
+    Totalprice,
+    TotalItem,
+    CityName,
+    Ispushsale,
+    Orderlocation,
+    Orderlongitude,
+    Orderlatitude,
+    MeetingID,
+    Inventoryprice,
+    Promodiscount,
+    Walletdiscount,
+    Omnipointsdiscount
+    Ordercancelreason
 from {{ source('main_db', 'Ordermaster') }}
+where COALESCE(status, 0) <> 3 
